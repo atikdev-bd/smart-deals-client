45af9c634355272fc10cbb8d58ecfd252f926046
atikdev <atikdev.bdd@gamil.com>
2025-12-25 12:02:00 +0600

    react project created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl text-amber-300 font-bold">
+        THIS IS SMART DEALS CLIENT SIDE
+      </div>
+    </>
+  );
+}
+
+export default App;
